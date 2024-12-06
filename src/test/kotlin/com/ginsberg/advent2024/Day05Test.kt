8c9ba682715fc4bff52c9683e64bea59c4566ab7
Todd Ginsberg <todd@ginsberg.com>
2024-12-06 17:56:13 -0500

    Fix labeling on tests

--- src/test/kotlin/com/ginsberg/advent2024/Day05Test.kt
+++ src/test/kotlin/com/ginsberg/advent2024/Day05Test.kt
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-@DisplayName("Day 4")
+@DisplayName("Day 5")
 class Day05Test {
 
     @Nested
--- src/test/kotlin/com/ginsberg/advent2024/Day06Test.kt
+++ src/test/kotlin/com/ginsberg/advent2024/Day06Test.kt
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-@DisplayName("Day 4")
+@DisplayName("Day 6")
 class Day06Test {
 
     // Arrange
